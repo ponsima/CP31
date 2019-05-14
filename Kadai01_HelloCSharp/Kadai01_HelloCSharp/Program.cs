af46e0216e6d591080445f527f02787dbe1fa20a
ponsima <ponsima1140@gmail.com>
2019-05-14 11:06:32 +0900

    first commit

---
+++ Kadai01_HelloCSharp/Kadai01_HelloCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kadai01_HelloCSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("はろー");
+        }
+    }
+}
