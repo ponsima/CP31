cf9713a7b74752cf550ac4ff5710a5caf643ae18
ponsima <ponsima1140@gmail.com>
2019-05-15 09:34:42 +0900

    [new] プロジェクト作成

---
+++ 0514_Form/0514_Form/FormMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0514_Form
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //初期化用のメソッド
+            this.Text = "製品名";
+
+            //プロパティウィンドウで操作できる内容は、
+            //コード（プログラム）で全てできる。
+            //minimizeをfalseにしてみよう。
+            MinimizeBox = false;
+
+            //this
+            //自インスタンス（オブジェクト）
+            //を指し示す。
+            //thisを利用するか、しないかは自由。
+            //S水はthis推奨
+            //this.と定義されていると、
+            //・自分の持ち物（プロパティorメソッド）
+            //と容易に判断できる
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
